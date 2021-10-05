6c4090c7877358b1abed86ee46db90e8f18ad152
Eugene Shevchenko <evgeny.shevchenko@flant.com>
2021-10-05 13:28:12 +0300

    split logic in files

--- src/format.ts
+++ src/format.ts
@@ -1,11 +1,18 @@
 import * as yaml from "js-yaml"
 import { ChangesByModule } from "./parse"
 
-export function formatMarkdown(milestone: string, body: string): string {
+export function formatMarkdown(milestone: string, body: ChangesByModule): string {
 	const header = `## Changelog ${milestone}`
-	return [header, body].join("\r\n\r\n")
+	return [header, formatYaml(body)].join("\r\n\r\n")
 }
-export function formatYaml(changesByModule: ChangesByModule): string {
-	// TODO UNKNOWN should come first
-	return yaml.dump(changesByModule, { sortKeys: true, lineWidth: 100 })
+export function formatYaml(body: ChangesByModule): string {
+	// TODO 'UNKNOWN' module should come first to highlight errors: `sortKeys` can be a function
+	const opts = {
+		sortKeys: true,
+		lineWidth: 100,
+		forceQuotes: false,
+		quotingType: "'",
+	} as yaml.DumpOptions
+
+	return yaml.dump(body, opts)
 }
