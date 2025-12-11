ba4af560757991db960926bb97fca4902d7312c5
Vladimir Portnov <vladimir.portnov@flant.com>
2025-12-11 15:23:09 +0300

    add throttling
    Signed-off-by: Vladimir Portnov <vladimir.portnov@flant.com>

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,8 @@
-import * as github from "@actions/github"
 import { Octokit } from "@octokit/core"
 import { PaginateInterface } from "@octokit/plugin-paginate-rest"
+import { throttling } from "@octokit/plugin-throttling"
 import { Api } from "@octokit/plugin-rest-endpoint-methods/dist-types/types"
+import { getOctokitOptions, GitHub } from "@actions/github/lib/utils"
 
 export type Pull = {
 	url: string
@@ -22,7 +23,32 @@ export class Client {
 
 	constructor(repo: string, token: string) {
 		this.repo = repo
-		this.octokit = github.getOctokit(token)
+		const octokit = GitHub.plugin(throttling)
+		this.octokit = new octokit(getOctokitOptions(token, {
+			throttle: {
+				onRateLimit: (retryAfter, options, octokit, retryCount) => {
+					octokit.log.warn(
+						`Request quota exhausted for request ${options.method} ${options.url}`,
+					);
+
+					if (retryCount < 3) {
+						octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+						return true;
+					}
+				},
+				onSecondaryRateLimit: (retryAfter, options, octokit, retryCount) => {
+					// does not retry, only logs a warning
+					octokit.log.warn(
+						`SecondaryRateLimit detected for request ${options.method} ${options.url}`,
+					);
+
+					if (retryCount < 3) {
+						octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+						return true;
+					}
+				},
+			},
+		}))
 	}
 
 	async getMilestonePulls(milestone: string): Promise<Pull[]> {
